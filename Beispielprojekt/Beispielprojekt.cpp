4632e71f1ee3c2fc7c9309f7df6e6ded8cdb51b4
florianhofmann <Florian.hofmann.familie@gmail.com>
2020-10-25 16:09:20 +0100

    Auto

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -17,10 +17,14 @@ class GameWindow : public Gosu::Window
 public:
 	Gosu::Image bild_hintergrund;
 	Gosu::Image bild_auto_türkis;
+	Gosu::Image bild_auto_rot;
+	Gosu::Image bild_auto_rot2;
 	GameWindow()
 		: Window(1920, 1080),
 		bild_hintergrund("Hintergrund.png"),
-		bild_auto_türkis("auto_tuerkis.png")
+		bild_auto_türkis("auto_tuerkis.png"),
+		bild_auto_rot("auto_rot.png")
+		bild_auto_rot("auto_rot2.png")
 	{
 		set_caption("Gosu Tutorial mit Git");
 	}
@@ -30,11 +34,14 @@ public:
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
 	int x = 2000;
 	int y = 750;
-
+	int z = 2000;
+	int o = 2000;
 	void draw() override
 	{
 		bild_hintergrund.draw_rot(0, 0, 0, 0, 0, 0);
 		bild_auto_türkis.draw_rot(x, y, 0, 0, 0, 0);
+		bild_auto_rot.draw_rot(z+1000, y, 0, 0, 0, 0);
+		bild_auto_rot2.draw_rot(o + 1000, y, 0, 0, 0, 0);
 	}
 
 	// Wird 60x pro Sekunde aufgerufen
@@ -44,8 +51,18 @@ public:
 	void update() override
 	{
 		x = (x - 5);
-			if (x < -300) {
-				x = 2000;
+			if (x < -1500) {
+				x = 3000;
+			}
+
+			z = (z - 5);
+			if (z < -1500) {
+				x = 3000;
+			
+
+			x = (x - 5);
+			if (x < -1500) {
+				x = 3000;
 			}
 
 		b = input().mouse_y();
