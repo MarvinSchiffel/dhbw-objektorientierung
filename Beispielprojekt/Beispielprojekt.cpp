47b9685450fadab97461fa08705653a242627260
Marvin Schiffel <Marvin.Schiffel@web.de>
2020-10-25 18:37:19 +0100

    Mit Lebensanzeige 25.10.2020

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -18,11 +18,13 @@ public:
 	Gosu::Image bild_hintergrund;
 	Gosu::Image bild_auto_türkis;
 	Gosu::Image bild_Oma1;
+	Gosu::Image bild_Lebensanzeige;
 	GameWindow()
 		: Window(1920, 1080),
 		bild_hintergrund("Hintergrund.png"),
 		bild_auto_türkis("auto_tuerkis.png"),
-		bild_Oma1("Oma1.png")
+		bild_Oma1("Oma1.png"),
+		bild_Lebensanzeige("Lebensanzeigebasic.png")
 	{
 		set_caption("Gosu Tutorial mit Git");
 	}
@@ -33,34 +35,33 @@ public:
 	int x_Auto1 = 2000;
 	int x_Auto2 = -150;
 	int x_Oma1 = 2000;
-	int y_Oberespur = 640;
+	int y_Oberespur = 620;
 	int y_Unterespur = 800;
 	int y_Gehweg = 430;
 	
-	double c = (a % 2);
+	
 
 
 	void draw() override
 	{
 		bild_hintergrund.draw_rot(0, 0, 0, 0, 0, 0);
-		if (c=0) {
-			bild_auto_türkis.draw_rot(x_Auto1, y_Oberespur, 2, 0, 0, 0, 1, 1);
-		}
+
+		bild_Lebensanzeige.draw_rot(0, 0, 1, 0,0,0);
 
 		bild_Oma1.draw_rot(x_Oma1, y_Gehweg, 1, 0, 0, 0, 1, 1);
+
+		bild_auto_türkis.draw_rot(x_Auto1, y_Oberespur, 2, 0, 0, 0, 1, 1);
+		
 		bild_auto_türkis.draw_rot(x_Auto2, y_Unterespur, 3, 0, 0, 0, -1, 1);
 	}
 
 	// Wird 60x pro Sekunde aufgerufen
 	
-	double b = 0;
-	int a = 0;
 	void update() override
 	{
 		x_Auto1 = (x_Auto1 - 8);
 		if (x_Auto1 < -400) {
 			x_Auto1 = 2000;
-			a = a + 1;
 		}
 		x_Auto2 = (x_Auto2 + 10);
 		if (x_Auto2 > 2500) {
@@ -70,8 +71,6 @@ public:
 		if (x_Oma1 < -300) {
 			x_Oma1 = 2000;
 		}
-
-		b = input().mouse_y();
 
 	}
 };
