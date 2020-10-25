0b977586e559ad2f5d6c271a4d31a63039426100
Tim <Tim@TIM-PC>
2020-10-25 21:57:51 +0100

    oma.cpp

---
+++ Beispielprojekt/objekte.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "objekte.h"
+#include "Gosu/Gosu.hpp"
+#include <cmath>
+
+void objekte::laufen() {
+	if (status == 4) {
+		this.img = Gosu::Image("oma_steht.png");
+		this.img.draw_rot(x_Oma1, y_Gehweg, 1, 0, 0, 0, 1, 1);
+		status -= 1;
+	}
+	else if (status == 3) {
+		this.img = Gosu::Image("oma_schritt1.png");
+		this.img.draw_rot(x_Oma1, y_Gehweg, 1, 0, 0, 0, 1, 1);
+		status -= 1;
+	}
+	else if (status == 2) {
+		this.img = Gosu::Image("oma_schritt2.png");
+		this.img.draw_rot(x_Oma1, y_Gehweg, 1, 0, 0, 0, 1, 1);
+		status -= 1;
+	}
+	else if (status == 1) {
+		this.img = Gosu::Image("oma_schritt3.png");
+		this.img.draw_rot(x_Oma1, y_Gehweg, 1, 0, 0, 0, 1, 1);
+		status = 4;
+	}
+}
