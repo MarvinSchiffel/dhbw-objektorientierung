d8f95dba69e62230a30f59f250e78168b60c9cc3
Marvin Schiffel <Marvin.Schiffel@web.de>
2020-10-25 17:30:50 +0100

    Try and Error

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -17,14 +17,12 @@ class GameWindow : public Gosu::Window
 public:
 	Gosu::Image bild_hintergrund;
 	Gosu::Image bild_auto_türkis;
-	Gosu::Image bild_auto_rot;
-	Gosu::Image bild_auto_rot2;
+	Gosu::Image bild_Oma1;
 	GameWindow()
 		: Window(1920, 1080),
 		bild_hintergrund("Hintergrund.png"),
 		bild_auto_türkis("auto_tuerkis.png"),
-		bild_auto_rot("auto_rot.png")
-		bild_auto_rot("auto_rot2.png")
+		bild_Oma1("Oma1.png")
 	{
 		set_caption("Gosu Tutorial mit Git");
 	}
@@ -32,16 +30,19 @@ public:
 	// wird bis zu 60x pro Sekunde aufgerufen.
 	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
-	int x = 2000;
-	int y = 750;
-	int z = 2000;
-	int o = 2000;
+	int x_Auto1 = 2000;
+	int x_Auto2 = -150;
+	int x_Oma1 = 2000;
+	int y_Oberespur = 640;
+	int y_Unterespur = 800;
+	int y_Gehweg = 430;
+
 	void draw() override
 	{
 		bild_hintergrund.draw_rot(0, 0, 0, 0, 0, 0);
-		bild_auto_türkis.draw_rot(x, y, 0, 0, 0, 0);
-		bild_auto_rot.draw_rot(z+1000, y, 0, 0, 0, 0);
-		bild_auto_rot2.draw_rot(o + 1000, y, 0, 0, 0, 0);
+		bild_auto_türkis.draw_rot(x_Auto1, y_Oberespur, 2, 0, 0, 0, 1, 1);
+		bild_Oma1.draw_rot(x_Oma1, y_Gehweg, 1, 0, 0, 0, 1, 1);
+		bild_auto_türkis.draw_rot(x_Auto2, y_Unterespur, 3, 0, 0, 0, -1, 1);
 	}
 
 	// Wird 60x pro Sekunde aufgerufen
@@ -50,20 +51,18 @@ public:
 
 	void update() override
 	{
-		x = (x - 5);
-			if (x < -1500) {
-				x = 3000;
-			}
-
-			z = (z - 5);
-			if (z < -1500) {
-				x = 3000;
-			
-
-			x = (x - 5);
-			if (x < -1500) {
-				x = 3000;
-			}
+		x_Auto1 = (x_Auto1 - 8);
+		if (x_Auto1 < -400) {
+			x_Auto1 = 2000;
+		}
+		x_Auto2 = (x_Auto2 + 10);
+		if (x_Auto2 > 2500) {
+			x_Auto2 = -300;
+		}
+		x_Oma1 = (x_Oma1 - 1);
+		if (x_Oma1 < -300) {
+			x_Oma1 = 2000;
+		}
 
 		b = input().mouse_y();
 
