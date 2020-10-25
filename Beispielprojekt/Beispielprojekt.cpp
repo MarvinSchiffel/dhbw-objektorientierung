90ae9257f3cfcbf024cebe83ad6decac2d4be1df
Marvin Schiffel <Marvin.Schiffel@web.de>
2020-10-25 15:01:29 +0100

    Programm nur mit Hintergrundbild

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -16,13 +16,10 @@ class GameWindow : public Gosu::Window
 {
 public:
 	Gosu::Image bild_rakete;
-	Gosu::Image bild_planet_1;
-	Gosu::Image bild_planet_2;
+	Gosu::Image bild_hintergrund;
 	GameWindow()
 		: Window(1920, 1080),
-		bild_rakete("rakete.png"),
-		bild_planet_1("planet1.png"),
-		bild_planet_2("planet2.png")
+		bild_hintergrund("Hintergrund.png")
 	{
 		set_caption("Gosu Tutorial mit Git");
 	}
@@ -30,30 +27,12 @@ public:
 	// wird bis zu 60x pro Sekunde aufgerufen.
 	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
-	int x = 500;
-	int y = 300;
-	int xp1 = 400;
-	int yp1 = 300;
-	int xp2 = 1200;
-	int yp2 = 500;
-	int rpos1 = 0;
-	int x2 = 1200;
-	int y2 = 600;
+	int x = 0;
+	int y = 0;
+
 	void draw() override
 	{
-		bild_planet_1.draw_rot(xp1, yp1, 0.0, 0.0, 0.5, 0.5);
-		bild_planet_2.draw_rot(xp2, yp2, 0.0, 0.0, 0.5, 0.5);
-		if (rpos1 = 0) {
-			bild_rakete.draw_rot(x, y, 1, a, 20, 0.5);
-
-			rpos1 = 1;
-		}
-		else {
-			bild_rakete.draw_rot(x2, y2, 1, a, -20, 0.5);
-
-			rpos1 = 0;
-		}
-
+		bild_hintergrund.draw_rot(0, 0, 0, 0, 0, 0);
 
 	}
 
