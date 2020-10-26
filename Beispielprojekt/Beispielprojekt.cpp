7c5cd9dbb51215e562bde38a52e126b73655d71f
Marvin Schiffel <Marvin.Schiffel@web.de>
2020-10-26 17:52:57 +0100

    New

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <iostream>
 
+#include "objekte.h"
+
 using namespace std;
 
 // Simulationsgeschwindigkeit
@@ -14,18 +16,26 @@ const double DT = 100.0;
 class GameWindow : public Gosu::Window
 {
 public:
-	Gosu::Image bild_hintergrund;
+
+	
+	//Bildnamen initialisieren
+	Gosu::Image bild_karohintergrund;
+	Gosu::Image bild_straßenbild;
 	Gosu::Image bild_auto_türkis;
 	Gosu::Image bild_auto_türkis2;
+	Gosu::Image bild_auto_rot;
 	Gosu::Image bild_oma_steht;
 	Gosu::Image bild_oma_schritt1;
 	Gosu::Image bild_oma_schritt2;
 	Gosu::Image bild_Lebensanzeige;
-	GameWindow()
-		: Window(1920, 1080),
-		bild_hintergrund("Hintergrund.png"),
+	
+	//Bildnamen mit Bilddaten initialisieren
+	GameWindow() : Window(1920, 1080),
+		bild_karohintergrund("karohintergrund.png"),
+		bild_straßenbild("strassenbild.png"),
 		bild_auto_türkis("auto_tuerkis.png"),
 		bild_auto_türkis2("auto_tuerkis2.png"),
+		bild_auto_rot("auto_rot.png"),
 		bild_oma_steht("oma_steht.png"),
 		bild_oma_schritt1("oma_schritt1.png"),
 		bild_oma_schritt2("oma_schritt2.png"),
@@ -39,46 +49,53 @@ public:
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
 	int x_Auto1 = 2000;
 	int x_Auto2 = -150;
-	int x_oma1 = 2000;
+	double x_oma = 0;
+	double y_oma = 0;
+	double v_oma = 5;
+
 	int y_Oberespur = 620;
 	int y_Unterespur = 800;
 	int y_gehweg = 430;
 	int status = 20;
-	int status_auto1 = 12;
+	int status_auto1 = 20;
 	
+	//Aufruf von draw Funktionen
 	void draw() override
 	{
-		bild_hintergrund.draw_rot(0, 0, 0, 0, 0, 0);
+		bild_karohintergrund.draw_rot(0, 0, 0, 0, 0, 0);
 
-		bild_Lebensanzeige.draw_rot(0, 0, 1, 0,0,0);
+		bild_straßenbild.draw_rot(0, 0, 1, 0, 0, 0);
 
-		if (status_auto1 >= 12) {
-			bild_auto_türkis.draw_rot(x_Auto1, y_Oberespur, 2, 0, 0, 0, 1, 1);
-			status_auto1 -= 1;
-		}
-		else if(status_auto1 >= 6){
-			bild_auto_türkis2.draw_rot(x_Auto1, y_Oberespur, 2, 0, 0, 0, 1, 1);
-			status_auto1 = 12;
-		}
+		bild_Lebensanzeige.draw_rot(0, 0, 2, 0,0,0);
 
-		bild_auto_türkis.draw_rot(x_Auto2, y_Unterespur, 3, 0, 0, 0, -1, 1);
+		bild_auto_türkis.draw_rot(x_Auto1, y_Oberespur, 3, 0, 0, 0, 1, 1);
 
+		bild_auto_türkis2.draw_rot(x_Auto1, y_Oberespur, 3, 0, 0, 0, 1, 1);
+		
+		bild_auto_rot.draw_rot(x_Auto2, y_Unterespur, 4, 0, 0, 0, -1, 1);
+
+		if (input().down(Gosu::KB_S) || input().down(Gosu::KB_W) || input().down(Gosu::KB_D) || input().down(Gosu::KB_A)) {
 			if (status >= 20) {
-				bild_oma_steht.draw_rot(x_oma1, y_gehweg, 1, 0, 0, 0);
+				bild_oma_steht.draw_rot(x_oma, y_oma, 1, 0, 1, 0,-1,1);
 				status -= 1;
 			}
 			else if (status >= 15) {
-				bild_oma_schritt1.draw_rot(x_oma1, y_gehweg, 1, 0, 0, 0);
+				bild_oma_schritt1.draw_rot(x_oma, y_oma, 1, 0, 1, 0,-1,1);
 				status -= 1;
 			}
 			else if (status >= 10) {
-				bild_oma_steht.draw_rot(x_oma1, y_gehweg, 1, 0, 0, 0);
+				bild_oma_steht.draw_rot(x_oma, y_oma, 1, 0, 1, 0,-1,1);
 				status -= 1;
 			}
 			else if (status >= 5) {
-				bild_oma_schritt2.draw_rot(x_oma1, y_gehweg, 1, 0, 0, 0);
+				bild_oma_schritt2.draw_rot(x_oma, y_oma, 1, 0, 1, 0,-1,1);
 				status = 20;
 			}
+			
+		}
+		else {
+			bild_oma_steht.draw_rot(x_oma, y_oma, 1, 0, 1, 0, -1, 1);
+		}
 
 	}
 
@@ -92,12 +109,26 @@ public:
 		}
 		x_Auto2 = (x_Auto2 + 10);
 		if (x_Auto2 > 2500) {
-			x_Auto2 = -300;
+			x_Auto2 = 2000;
 		}
-		x_oma1 = (x_oma1 - 2);
-		if (x_oma1 < -300) {
-			x_oma1 = 2000;
+		//Oma läuft nach unten
+		if (input().down(Gosu::KB_S)) {
+			y_oma += v_oma;
 		}
+		//Oma läuft nach oben
+		if (input().down(Gosu::KB_W)) {
+			y_oma -= v_oma;
+		}
+		//Oma läuft vorwärts
+		if (input().down(Gosu::KB_D)) {
+			x_oma += v_oma;
+		}
+		//Oma läuft rückwärts
+		if (input().down(Gosu::KB_A)) {
+			x_oma -= v_oma;
+		}
+
+		
 
 	}
 };
